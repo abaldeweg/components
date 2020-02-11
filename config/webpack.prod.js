15937a5d4a6f3e4f1ceb53e584e00f46f347d914
André Baldeweg <kontakt@andrebaldeweg.de>
2020-02-11 14:14:37 +0100

    build: add prod env

---
+++ config/webpack.prod.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, './../dist'),
+    publicPath: '/',
+    filename: 'components.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 4096,
+          name: path.posix.join('img/[name].[hash].[ext]')
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: path.posix.join('img/[name].[hash].[ext]')
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devtool: false,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true
+      })
+    ]
+  }
+}
