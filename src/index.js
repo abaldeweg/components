65baecf7c8315c836073400c77924dace7a276a8
André Baldeweg <kontakt@andrebaldeweg.de>
2020-02-11 18:42:25 +0100

    feature export navigation-bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Notification from './components/notification/notification'
+import NotificationBar from './components/notification-bar/notification-bar'
 
-export { Notification }
+export { Notification, NotificationBar }
 
 const plugin = {
   install(Vue) {
     Vue.component('b-notification', Notification)
+    Vue.component('b-notification-bar', NotificationBar)
   }
 }
 
