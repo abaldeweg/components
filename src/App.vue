f3c2763b8046b4c0b5aa38d2d453198640b95a7a
André Baldeweg <kontakt@andrebaldeweg.de>
2020-02-12 00:12:09 +0100

    feature: switch between light and dark

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <b-app>
     <router-link :to="{ name: 'index' }">Home</router-link>
     <router-view />
+    <button @click="toggleDarkMode">Toggle DarkMode</button>
   </b-app>
 </template>
 
@@ -12,6 +13,15 @@ export default {
   name: 'app',
   components: {
     BApp
+  },
+  methods: {
+    toggleDarkMode() {
+      if (document.documentElement.hasAttribute('dark')) {
+        document.documentElement.removeAttribute('dark')
+      } else {
+        document.documentElement.setAttribute('dark', true)
+      }
+    }
   }
 }
 </script>
