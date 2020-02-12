e7ba03d04e18ea6b9eb583f76b50d733fea18e68
André Baldeweg <kontakt@andrebaldeweg.de>
2020-02-12 14:18:45 +0100

    feature: add vue-holderjs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
+import VueHolder from 'vue-holderjs'
+
+Vue.use(VueHolder)
 
 Vue.config.productionTip = false
 
