8e165be036921738d314c8734f43e51524099a12
André Baldeweg <kontakt@andrebaldeweg.de>
2020-02-12 21:15:09 +0100

    feature: icon

--- src/components/notification/notification.vue
+++ src/components/notification/notification.vue
@@ -9,7 +9,19 @@
     }"
     v-if="show"
   >
-    <span class="notification_close" v-if="hidable" @click="close">X</span>
+    <span class="notification_close" v-if="hidable" @click="close">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        viewBox="0 0 50 50"
+        class="icon"
+      >
+        <path
+          d="M44.987 9.643l-15.35 15.349 15.35 15.35-4.685 4.684-15.349-15.35-15.35 15.35-4.629-4.63 15.35-15.349-15.35-15.35L9.66 5.014l15.35 15.35 15.348-15.35z"
+        />
+      </svg>
+    </span>
     <h2 class="notification_title">{{ title }}</h2>
     <p class="notification_entry"><slot /></p>
   </aside>
