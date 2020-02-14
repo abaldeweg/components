56e8f0a7f3daffc052e200c2b7670b66292dbedd
André Baldeweg <kontakt@andrebaldeweg.de>
2020-02-14 16:01:14 +0100

    fix: remove container

--- src/views/BButton.vue
+++ src/views/BButton.vue
@@ -1,28 +1,22 @@
 <template>
   <article>
-    <section class="container container_m">
-      <h1>Primary</h1>
-      <b-button type="primary">Button Primary</b-button>
-      <b-button type="primary_danger">Button Danger</b-button>
-      <br />
-      <br />
-      <b-button type="primary_wide">Button Wide</b-button>
-    </section>
+    <h2>Primary</h2>
+    <b-button type="primary">Button Primary</b-button>
+    <b-button type="primary_danger">Button Danger</b-button>
+    <br />
+    <br />
+    <b-button type="primary_wide">Button Wide</b-button>
 
-    <section class="container container_m">
-      <h1>Outline</h1>
-      <b-button type="outline">Button Outline</b-button>
-      <b-button type="outline_danger">Button Danger</b-button>
-      <br />
-      <br />
-      <b-button type="outline_wide">Button Wide</b-button>
-    </section>
+    <h2>Outline</h2>
+    <b-button type="outline">Button Outline</b-button>
+    <b-button type="outline_danger">Button Danger</b-button>
+    <br />
+    <br />
+    <b-button type="outline_wide">Button Wide</b-button>
 
-    <section class="container container_m">
-      <h1>Text</h1>
-      <b-button type="text">Button Text</b-button>
-      <b-button type="text_danger">Button Danger</b-button>
-    </section>
+    <h2>Text</h2>
+    <b-button type="text">Button Text</b-button>
+    <b-button type="text_danger">Button Danger</b-button>
   </article>
 </template>
 
