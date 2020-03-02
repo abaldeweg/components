9e63986936507618e1475e699b9e611cb6c27dda
André Baldeweg <kontakt@andrebaldeweg.de>
2020-03-02 12:36:51 +0100

    fix: icons must be visible in dark mode

--- src/components/BNotification/BNotification.vue
+++ src/components/BNotification/BNotification.vue
@@ -80,18 +80,28 @@ html[dark] .notification {
   background: var(--color-neutral-00);
 }
 html[dark] .notification_neutral {
-  background: var(--color-neutral-10);
+  background: var(--color-neutral-02);
+  color: var(--color-neutral-10);
 }
 .notification_caution {
   border: 2px solid var(--color-accent-yellow-10);
   background: var(--color-accent-yellow-00);
 }
+html[dark] .notification_caution {
+  background: var(--color-accent-yellow-05);
+}
 .notification_error {
   border: 2px solid var(--color-accent-red-10);
   background: var(--color-accent-red-00);
 }
+html[dark] .notification_error {
+  background: var(--color-accent-red-05);
+}
 .notification_success {
   border: 2px solid var(--color-accent-green-10);
   background: var(--color-accent-green-00);
+}
+html[dark] .notification_success {
+  background: var(--color-accent-green-05);
 }
 </style>
