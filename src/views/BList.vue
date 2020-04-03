a67cc9862bf34002a22a44634e450dbe3bb3adb8
André Baldeweg <kontakt@andrebaldeweg.de>
2020-04-03 19:11:31 +0200

    feature: add form example

--- src/views/BList.vue
+++ src/views/BList.vue
@@ -37,6 +37,29 @@
         </b-dropdown-item>
       </template>
     </b-list>
+
+    <b-list>
+      <template #title>
+        <b-form class="form">
+          <div class="form_group">
+            <div class="form_item">
+              <label for="title" class="form_label visuallyHidden">Title</label>
+            </div>
+            <div class="form_item">
+              <input type="text" class="form_input" id="title" />
+            </div>
+          </div>
+        </b-form>
+      </template>
+      <template #options>
+        <b-dropdown-item>
+          Edit 2
+        </b-dropdown-item>
+        <b-dropdown-item>
+          Remove
+        </b-dropdown-item>
+      </template>
+    </b-list>
   </article>
 </template>
 
@@ -44,13 +67,15 @@
 import BList from '../components/BList/BList'
 import BListSeperator from '../components/BList/BListSeperator'
 import BDropdownItem from '../components/BDropdown/BDropdownItem'
+import BForm from '../components/BForm/BForm'
 
 export default {
   name: 'b-list-view',
   components: {
     BList,
     BListSeperator,
-    BDropdownItem
+    BDropdownItem,
+    BForm
   }
 }
 </script>
