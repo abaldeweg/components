a1ac8063815b5a5574462b9c0fb4ed405ac5af74
André Baldeweg <kontakt@andrebaldeweg.de>
2020-07-25 00:02:29 +0200

    fix: spinner with transparent borders

--- src/components/BSpinner/BSpinner.vue
+++ src/components/BSpinner/BSpinner.vue
@@ -35,24 +35,24 @@ export default {
 .spinner_s {
   border-top: 2px solid var(--color-primary-10);
   border-bottom: 2px solid var(--color-primary-10);
-  border-left: 2px solid var(--color-neutral-00);
-  border-right: 2px solid var(--color-neutral-00);
+  border-left: 2px solid transparent;
+  border-right: 2px solid transparent;
   width: 20px;
   height: 20px;
 }
 .spinner_m {
   border-top: 4px solid var(--color-primary-10);
   border-bottom: 4px solid var(--color-primary-10);
-  border-left: 4px solid var(--color-neutral-00);
-  border-right: 4px solid var(--color-neutral-00);
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
   width: 40px;
   height: 40px;
 }
 .spinner_l {
   border-top: 6px solid var(--color-primary-10);
   border-bottom: 6px solid var(--color-primary-10);
-  border-left: 6px solid var(--color-neutral-00);
-  border-right: 6px solid var(--color-neutral-00);
+  border-left: 6px solid transparent;
+  border-right: 6px solid transparent;
   width: 60px;
   height: 60px;
 }
