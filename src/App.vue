7c47f15db4b037a5a7cc27241e2f5f47365fe68c
André Baldeweg <kontakt@andrebaldeweg.de>
2020-07-24 18:20:25 +0200

    feature: admin interface

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,64 @@
 <template>
   <b-app>
-    <router-link :to="{ name: 'index' }">Home</router-link>
     <b-content>
       <router-view />
     </b-content>
+
+    <div
+      class="components"
+      :class="{
+        isTop: position === 'top',
+        isBottom: position === 'bottom',
+      }"
+    >
+      <router-link :to="{ name: 'index' }">Home</router-link>
+      <b-theme />
+      <button @click="togglePosition">Top/Bottom</button>
+    </div>
   </b-app>
 </template>
 
 <script>
 import BApp from './components/BApp/BApp'
 import BContent from './components/BContent/BContent'
+import BTheme from './components/BTheme/BTheme'
 
 export default {
   name: 'app',
   components: {
     BApp,
     BContent,
+    BTheme,
+  },
+  data() {
+    return {
+      position: 'bottom',
+    }
+  },
+  methods: {
+    togglePosition: function () {
+      this.position = this.position === 'top' ? 'bottom' : 'top'
+    },
   },
 }
 </script>
+
+<style scoped>
+.components {
+  position: fixed;
+  right: 0;
+  width: 200px;
+  border: 1px solid var(--color-neutral-04);
+  border-radius: 10px;
+  background: var(--color-neutral-00);
+  padding: 20px;
+  margin: 20px;
+  box-sizing: border-box;
+}
+.components.isTop {
+  top: 0;
+}
+.components.isBottom {
+  bottom: 0;
+}
+</style>
