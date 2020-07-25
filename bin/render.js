6e560d8b3fe33cef37c5d767266fc31822e78ef7
André Baldeweg <kontakt@andrebaldeweg.de>
2020-07-25 14:02:27 +0200

    feature: new script for generating new component templates

---
+++ bin/render.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const ejs = require('ejs');
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const { kebabCase, camelCase, upperFirst, snakeCase } = require('lodash')
+
+const name = kebabCase(process.argv[2]) || 'component'
+const component = 'B' + upperFirst(camelCase(name))
+const filename = 'B' + upperFirst(camelCase(name)) + '.vue'
+const data = {
+  name: name,
+  cssClass: snakeCase(name),
+  component: component
+};
+const options = {};
+
+// component
+renderFile(
+  './bin/component',
+  data,
+  options,
+  './src/components/' + component,
+  './src/components/' + component + '/' + filename
+);
+// view
+renderFile(
+  './bin/view',
+  data,
+  options,
+  './src/views',
+  './src/views/' + filename
+);
+
+function renderFile(source, data, options, mkdir, file) {
+  ejs.renderFile(source, data, options, function (err, str) {
+    mkdirp(mkdir)
+      .then(function () {
+        fs.writeFile(file, str, function (err) {
+          if (err) console.error(err)
+        })
+      })
+  });
+}
