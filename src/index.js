826c1a5e48434531536b0e4e470a69d16a0fb2d7
André Baldeweg <kontakt@andrebaldeweg.de>
2020-08-18 15:30:04 +0200

    fix: export tooltip

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ import BToggle from './components/BToggle/BToggle'
 import BModal from './components/BModal/BModal'
 import BScrollToBottom from './components/BScrollToBottom/BScrollToBottom'
 import BLocale from './components/BLocale/BLocale'
+import BTooltip from './components/BTooltip/BTooltip'
 import themes from './services/theme'
 import focus from './directives/focus'
 import tooltip from './directives/tooltip'
@@ -81,6 +82,7 @@ export {
   BModal,
   BScrollToBottom,
   BLocale,
+  BTooltip,
   notification,
   validator,
 }
@@ -125,6 +127,7 @@ export default {
     Vue.component('b-modal', BModal)
     Vue.component('b-scroll-to-bottom', BScrollToBottom)
     Vue.component('b-locale', BLocale)
+    Vue.component('b-tooltip', BTooltip)
     Vue.directive('focus', focus)
     Vue.directive('tooltip', tooltip)
     Vue.themes = themes
