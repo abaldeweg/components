42219e9729d18522702a85852d2a7e45ba93a372
André Baldeweg <kontakt@andrebaldeweg.de>
2021-06-23 16:12:21 +0200

    refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import i18n from './i18n'
 import VueHolder from 'vue-holderjs'
 import themes from './services/theme'
 import meta from './meta'
@@ -16,8 +14,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
-  i18n,
   meta,
   render: (h) => h(App),
 }).$mount('#app')
