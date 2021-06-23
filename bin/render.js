80c095d31ed10f06bed22be22417a9a81103b4f6
André Baldeweg <kontakt@andrebaldeweg.de>
2021-06-23 15:59:42 +0200

    lint

--- bin/render.js
+++ bin/render.js
@@ -2,7 +2,7 @@
 
 'use strict'
 
-const ejs = require('ejs');
+const ejs = require('ejs')
 const fs = require('fs')
 const mkdirp = require('mkdirp')
 const { kebabCase, camelCase, upperFirst, snakeCase } = require('lodash')
@@ -13,9 +13,9 @@ const filename = 'B' + upperFirst(camelCase(name)) + '.vue'
 const data = {
   name: name,
   cssClass: snakeCase(name),
-  component: component
-};
-const options = {};
+  component: component,
+}
+const options = {}
 
 // component
 renderFile(
@@ -24,7 +24,7 @@ renderFile(
   options,
   './src/components/' + component,
   './src/components/' + component + '/' + filename
-);
+)
 // view
 renderFile(
   __dirname + '/view',
@@ -32,17 +32,16 @@ renderFile(
   options,
   './src/views',
   './src/views/' + filename
-);
+)
 
 function renderFile(source, data, options, mkdir, file) {
   ejs.renderFile(source, data, options, function (err, str) {
-    mkdirp(mkdir)
-      .then(function () {
-        fs.writeFile(file, str, function (err) {
-          if (err) console.error(err)
-        })
+    mkdirp(mkdir).then(function () {
+      fs.writeFile(file, str, function (err) {
+        if (err) console.error(err)
       })
-  });
+    })
+  })
 }
 
 console.log('Add the new component to the Index view.')
