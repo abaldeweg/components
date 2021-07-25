ad80ba9d7ac4d88e41bc54c8cd65d1227390fe1e
André Baldeweg <kontakt@andrebaldeweg.de>
2021-07-25 17:28:08 +0200

    refactor renderer

--- bin/render.js
+++ bin/render.js
@@ -15,34 +15,19 @@ const data = {
   cssClass: snakeCase(name),
   component: component,
 }
-const options = {}
 
-// component
-renderFile(
-  __dirname + '/templates/component',
-  data,
-  options,
-  './src/components/' + component,
-  './src/components/' + component + '/' + filename
-)
-// view
-renderFile(
-  __dirname + '/templates/view',
-  data,
-  options,
-  './src/views',
-  './src/views/' + filename
-)
-
-function renderFile(source, data, options, mkdir, file) {
-  ejs.renderFile(source, data, options, function (err, str) {
-    mkdirp(mkdir).then(function () {
-      fs.writeFile(file, str, function (err) {
-        if (err) console.error(err)
+const renderFile = (source, dir) => {
+  ejs.renderFile(source, data, {}, (err, str) => {
+    mkdirp(dir).then(() => {
+      fs.writeFile(dir + '/' + filename, str, (error) => {
+        if (error) console.error(error)
       })
     })
   })
 }
+
+renderFile(__dirname + '/templates/component', './src/components/' + component)
+renderFile(__dirname + '/templates/view', './src/views')
 
 console.log('Add the new component to the Index view.')
 console.log('Add the new component to the router')
